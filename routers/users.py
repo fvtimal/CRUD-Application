70b556308504c2d0281b1400f4a869e013c339ac
fvtimal <fatimal4tif@gmail.com>
2026-07-07 06:56:20 +0500

    rejects duplicate email addresses

--- routers/users.py
+++ routers/users.py
@@ -1,13 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from database import users
+from models import UserRegister
 
 router = APIRouter(
     prefix="/users",
-    tags=["users"]
+    tags=["Users"]
 )
 
 
-@router.post("/register")
-async def register():
-    return{
-        "message": "Registration endpoint working"
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register(user: UserRegister):
+
+    existing_user = await users.find_one(
+        {"email": user.email}
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered."
+        )
+
+    user_dict = user.model_dump()
+
+    await users.insert_one(user_dict)
+
+    return {
+        "message": "User Registered Successfully"
     }
