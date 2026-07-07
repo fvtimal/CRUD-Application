d38893d93d14f8ef4ee90f5199d19e12fd41a2e3
fvtimal <fatimal4tif@gmail.com>
2026-07-07 07:18:40 +0500

    fixed users.py

--- routers/users.py
+++ routers/users.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, HTTPException, status
 
 from database import users
 from models import UserRegister
+from security import hash_password
+
 
 router = APIRouter(
     prefix="/users",
@@ -13,18 +15,33 @@ router = APIRouter(
 async def register(user: UserRegister):
 
     existing_user = await users.find_one(
-        {"email": user.email}
+        {
+            "email": user.email
+        }
     )
 
     if existing_user:
         raise HTTPException(
             status_code=400,
-            detail="Email already registered."
+            detail="Email already registered"
         )
+
+
+    hashed_password = hash_password(
+        user.password
+    )
+
 
     user_dict = user.model_dump()
 
-    await users.insert_one(user_dict)
+
+    user_dict["password"] = hashed_password
+
+
+    await users.insert_one(
+        user_dict
+    )
+
 
     return {
         "message": "User Registered Successfully"
