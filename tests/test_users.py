7a2389307a8327d998467b1744a91df60ba5bb51
fvtimal <fatimal4tif@gmail.com>
2026-07-07 14:36:54 +0500

    testing uptil login

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,27 +5,46 @@ from main import app
 
 
 client = TestClient(app)
+#
+#
+# def test_home():
+#
+#     response = client.get("/")
+#
+#     assert response.status_code == 200
+#
+#     assert response.json() == {
+#         "message": "TODO API is running"
+#     }
+#
+# def test_register():
+#
+#     response = client.post(
+#         "/users/register",
+#         json={
+#             "username": "testuser",
+#             "email": "test@test.com",
+#             "password": "123456"
+#         }
+#     )
+#
+#     assert response.status_code == 201
 
 
-def test_home():
-
-    response = client.get("/")
-
-    assert response.status_code == 200
-
-    assert response.json() == {
-        "message": "TODO API is running"
-    }
-
-def test_register():
+def test_login():
 
     response = client.post(
-        "/users/register",
-        json={
-            "username": "testuser",
-            "email": "test@test.com",
+        "/users/login",
+        data={
+            "username": "test@test.com",
             "password": "123456"
         }
     )
 
-    assert response.status_code == 201
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert "access_token" in body
+
+    assert body["token_type"] == "bearer"
