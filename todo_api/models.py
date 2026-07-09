185cd334ee8fe1fcba75c0ac6b2ed8c888654667
fvtimal <fatimal4tif@gmail.com>
2026-07-09 10:28:30 +0500

    added field examples

--- todo_api/models.py
+++ todo_api/models.py
@@ -1,17 +1,26 @@
 #pydantc models
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserRegister(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
+    username: str = Field(
+        example="John"
+    )
+    email: EmailStr = Field(
+        example="john@gmail.com"
+    )
+    password: str = Field(
+        example="password123"
+    )
 
 
 
 class TodoCreate(BaseModel):
-    title: str
+    title: str = Field(
+        example="Study FastAPI"
+    )
+
     completed: bool = False
 
 
